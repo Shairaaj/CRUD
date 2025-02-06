d2f591986ac3f2f2668b597eda287df8ccf3bbe9
Shairaaj <iamshairaaj@gmail.com>
2025-02-06 19:16:09 +0530

    Second Commit

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,3 @@ app.listen(5000, ()=>{
 app.get("/",(req,res)=>{
     res.send("homePage");
 })
-
-echo "# CRUD" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Shairaaj/CRUD.git
-git push -u origin main
