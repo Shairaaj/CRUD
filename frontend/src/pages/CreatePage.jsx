890d349731b8af0abd6201b6aaee957d2ce48883
Shairaaj <iamshairaaj@gmail.com>
2025-02-10 21:46:25 +0530

    Creating Create page

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,72 @@
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  useColorMode,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState([
+    {
+      name: "",
+      price: "",
+      image: "",
+    },
+  ]);
+  const handleAddProduct=()=>{
+    console.log(newProduct);
+  }
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+          <Box
+            w={"full"}
+            bg={useColorModeValue("white", "gray.800")}
+            p={6}
+            rounded={"1g"}
+            shadow={"md"}
+          >
+            <VStack spacing={4}>
+              <Input
+                placeholder="Product Name"
+                name="name"
+                value={newProduct.name}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, name: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Price"
+                name="price"
+                value={newProduct.price}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, price: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Image URL"
+                name="image"
+                value={newProduct.image}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, image: e.target.value })
+                }
+              />
+              <Button colorScheme="blue" onClick={handleAddProduct} w="full">
+                Add Product
+              </Button>
+            </VStack>
+          </Box>
+        </Heading>
+      </VStack>
+    </Container>
+  );
+};
+
+export default CreatePage;
